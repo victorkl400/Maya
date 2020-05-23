e36fbc4adf682f3338d13c3d65d79e6e5b3c4a5b
Victor Emanuel <mrgoodye@gmail.com>
2020-05-23 14:35:45 -0300

    Updates

---
+++ precache-manifest.d81ffe57ed81eb80c0a964358165182a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df54c82dd537d9de308ca74395371aa7",
+    "url": "/Maya/index.html"
+  },
+  {
+    "revision": "236861726510439d9f93",
+    "url": "/Maya/static/css/2.8423e45c.chunk.css"
+  },
+  {
+    "revision": "f453cd5fac8602125491",
+    "url": "/Maya/static/css/main.a743142f.chunk.css"
+  },
+  {
+    "revision": "236861726510439d9f93",
+    "url": "/Maya/static/js/2.b0aa85cc.chunk.js"
+  },
+  {
+    "revision": "f453cd5fac8602125491",
+    "url": "/Maya/static/js/main.03b69032.chunk.js"
+  },
+  {
+    "revision": "4580c56ef965e540c2db",
+    "url": "/Maya/static/js/runtime-main.ca00a42e.js"
+  },
+  {
+    "revision": "7fde600e99cad3b78b92176ff19637d2",
+    "url": "/Maya/static/media/logo.7fde600e.svg"
+  },
+  {
+    "revision": "246f613de79e0665967692b0cc7076d6",
+    "url": "/Maya/static/media/logoMaya_3.246f613d.svg"
+  },
+  {
+    "revision": "e206db69582c2547892c889c55392c8c",
+    "url": "/Maya/static/media/premium.e206db69.svg"
+  },
+  {
+    "revision": "4ee99756815cab01a5a52b4e28ffe513",
+    "url": "/Maya/static/media/solution_3.4ee99756.svg"
+  }
+]);
